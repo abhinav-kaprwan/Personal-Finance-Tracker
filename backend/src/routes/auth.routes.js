bbdd4282f26c77f36073ccd68b95c57d00427b9d
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2025-12-30 18:57:20 +0530

    Added login and register routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,7 +3,7 @@ import { register,login } from "../controllers/auth.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 import { authorizeRoles } from "../middlewares/role.middleware.js";
 const router = express.Router();
-
+// auth login and register routes
 router.post("/register", register);
 router.post("/login",login);
 
