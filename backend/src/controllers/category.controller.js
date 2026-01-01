d9d7d9f087acfcd0308be89c72557f43fa1eccab
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2026-01-01 10:54:01 +0530

    dissable reddis for category

--- backend/src/controllers/category.controller.js
+++ backend/src/controllers/category.controller.js
@@ -3,30 +3,9 @@ import redisClient from "../config/redis.js";
 
 export const getCategories = async (req, res) => {
   try {
-    const cacheKey = "categories:all";
-
-    if(redisClient){
-      try {
-        const cached = await redisClient.get(cacheKey);
-        if (cached) {
-          return res.json(JSON.parse(cached));
-        }
-      } catch (err) {
-        console.warn("Redis GET failed, skipping cache");
-      }
-    }
-
     const result = await pool.query(
       "SELECT id, name, type FROM categories ORDER BY name"
     );
-
-    if(redisClient){
-      redisClient.setEx(
-      cacheKey,
-      3600,
-      JSON.stringify(result.rows)
-    ).catch(() => {});;
-    }
 
     res.json(result.rows);
 
