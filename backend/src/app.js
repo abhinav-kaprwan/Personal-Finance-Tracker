b4636f1017ac65b5324bc0211cf8aec9e978e247
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2026-01-01 11:28:18 +0530

    fixed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://personal-finance-tracker-abhinav-kaprwans-projects.vercel.app/"
+      "https://personal-finance-tracker-abhinav-kaprwans-projects.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
